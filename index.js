e3ea8b1384b374fdeec1955e3ca395d1638b8aff
dharamrdev <dharamrajyadav6312@gmail.com>
2024-08-14 15:26:28 +0530

    Done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {generateRegistrationOptions} = require('@simplewebauthn/server');
+const {generateRegistrationOptions, verifyRegistrationResponse, generateAuthenticationOptions, verifyAuthenticationResponse} = require('@simplewebauthn/server');
 const crypto = require('node:crypto');
 
 if(!globalThis.crypto){
@@ -50,6 +50,66 @@ app.post("/register-challenge", async(req, res) => {
     challengeStore[userId] = challengePayload.challenge;
 
     return res.json({options: challengePayload})
+})
+
+app.post("/register-verify", async(req, res) => {
+    const {userId, cred} = req.body;
+
+    if(!userStore[userId]) return res.status(404).json({message: "User Not Found!"});
+
+    const user = userStore[userId];
+
+    const challenge = challengeStore[userId];
+
+    const verificationResult = await verifyRegistrationResponse({
+        expectedChallenge: challenge,
+        expectedOrigin: "http://localhost:8000",
+        expectedRPID: 'localhost',
+        response: cred
+    });
+
+    if(!verificationResult.verified) return res.json({error: "Could not verify"})
+    
+    userStore[userId].passkey = verificationResult.registrationInfo
+
+    return res.json({verified: true})
+
+})
+
+app.post("/login-challenge", async(req, res) => {
+    const {userId} = req.body;
+    console.log("userId login", userId)
+    if(!userStore[userId]) return res.status(404).json({message: "User Not Found!"});
+
+    const opts = await generateAuthenticationOptions({
+        rpID: 'localhost',
+    })
+
+    challengeStore[userId] = opts.challenge;
+
+    return res.json({options: opts})
+
+})
+
+app.post("/login-verify", async(req, res) => {
+    const {userId, cred} = req.body;
+    const user = userStore[userId];
+
+    if(!userStore[userId]) return res.json({error: "User Not Found"})
+
+    const challenge = challengeStore[userId];
+
+    const result = await verifyAuthenticationResponse({
+        expectedChallenge: challenge,
+        expectedOrigin: "http://localhost:8000",
+        expectedRPID: 'localhost',
+        response: cred,
+        authenticator: user.passkey
+    })
+
+    if(!result.verified) return res.json({error: "Something went wrong"})
+    
+    return res.json({success: true, userId})
 
 })
 
